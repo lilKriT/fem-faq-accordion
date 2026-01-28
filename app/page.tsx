b014d7663d3a377cf4189e77e0161d579f3f7b29
lilKriT <lilKriTwebDev@gmail.com>
2026-01-28 22:30:17 +0100

    Styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,42 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans">
-      <p>FAQs</p>
+    <section className="flex min-h-dvh justify-center items-center">
+      <div>
+        <p>FAQs</p>
 
-      <p>What is Frontend Mentor, and how will it help me?</p>
+        <p>What is Frontend Mentor, and how will it help me?</p>
 
-      <p>
-        Frontend Mentor offers realistic coding challenges to help developers
-        improve their frontend coding skills with projects in HTML, CSS, and
-        JavaScript. It's suitable for all levels and ideal for portfolio
-        building.
-      </p>
+        <p>
+          Frontend Mentor offers realistic coding challenges to help developers
+          improve their frontend coding skills with projects in HTML, CSS, and
+          JavaScript. It's suitable for all levels and ideal for portfolio
+          building.
+        </p>
 
-      <p>Is Frontend Mentor free?</p>
+        <p>Is Frontend Mentor free?</p>
 
-      <p>
-        Yes, Frontend Mentor offers both free and premium coding challenges,
-        with the free option providing access to a range of projects suitable
-        for all skill levels.
-      </p>
+        <p>
+          Yes, Frontend Mentor offers both free and premium coding challenges,
+          with the free option providing access to a range of projects suitable
+          for all skill levels.
+        </p>
 
-      <p>Can I use Frontend Mentor projects in my portfolio?</p>
+        <p>Can I use Frontend Mentor projects in my portfolio?</p>
 
-      <p>
-        Yes, you can use projects completed on Frontend Mentor in your
-        portfolio. It's an excellent way to showcase your skills to potential
-        employers!
-      </p>
+        <p>
+          Yes, you can use projects completed on Frontend Mentor in your
+          portfolio. It's an excellent way to showcase your skills to potential
+          employers!
+        </p>
 
-      <p>How can I get help if I'm stuck on a Frontend Mentor challenge?</p>
+        <p>How can I get help if I'm stuck on a Frontend Mentor challenge?</p>
 
-      <p>
-        The best place to get help is inside Frontend Mentor's Discord
-        community. There's a help channel where you can ask questions and seek
-        support from other community members.
-      </p>
-    </div>
+        <p>
+          The best place to get help is inside Frontend Mentor's Discord
+          community. There's a help channel where you can ask questions and seek
+          support from other community members.
+        </p>
+      </div>
+    </section>
   );
 }
